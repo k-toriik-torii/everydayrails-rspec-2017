b0b83058968e51175a0eb0eedddc72b7e63ebb0c
k-toriik-torii <k-torii@rhizome-e.com>
2021-07-29 22:57:43 +0900

    コントローラースペック

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -20,6 +20,11 @@ FactoryBot.define do
     trait :due_tomorrow do
       due_on 1.day.from_now
     end
+    
+    # 無効になっている
+    trait :invalid do
+      name nil
+    end
 
     # Factory inheritance examples ...
     #
